2fa57cc4aab3bfc52e029c0ad0c6444864168bf6
Jesper Noehr <jesper@noehr.org>
2020-07-07 06:01:53 +0200

    Percentage confirmed by population

--- daemon.py
+++ daemon.py
@@ -10,6 +10,21 @@ CONST_BASE_URL = 'https://api.covid19api.com'
 CONST_COUNTRY_URL = CONST_BASE_URL + '/total/country'
 CONST_CFG_FN = 'config.toml'
 
+POPULATION = {
+    'china': 1_403_409_560,
+    'brazil': 211_755_948,
+    'denmark': 5_824_857,
+    'france': 67_081_000,
+    'germany': 83_166_711,
+    'iran': 83_590_243,
+    'italy': 60_238_522,
+    'korea-south': 51_780_579,
+    'spain': 47_329_981,
+    'sweden': 10_345_449,
+    'united-kingdom': 66_796_807,
+    'united-states': 329_986_180,
+}
+
 if __name__ == '__main__':
     delay = 60
     config = {}
@@ -64,6 +79,7 @@ if __name__ == '__main__':
                         },
                         "time": dp["Date"],
                         "fields": {
+                            "confirmed_pct": (dp["Confirmed"] / POPULATION[country])*100,
                             "confirmed": dp["Confirmed"],
                             "deaths": dp["Deaths"],
                             "recovered": dp["Recovered"],
